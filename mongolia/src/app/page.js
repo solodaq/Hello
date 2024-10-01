6a7d0bcdbd8ea9bad209ed92025b531980ea8462
Munkh-Erdene Sukhbaatar <munkh-erdenesukhbaatar@Munkh-Erdenes-MacBook-Air.local>
2024-10-01 12:50:50 +0800

    my work is done

--- mongolia/src/app/page.js
+++ mongolia/src/app/page.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Footer />
       <p>Hello this part is made by MUNK-ERDENE</p>
       <p>And here we have sarahhH!!</p>
+      <h2 className="text-blue-300">My work is done by sARARARJAJR</h2>
     </div>
   );
 }
